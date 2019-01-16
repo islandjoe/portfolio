210465fbe373c844032112f48f3dfbc1ff82762f
Arthur Kho <islandjoe@gmail.com>
2019-01-16 15:25:34 +0200

    Take 4

---
+++ reactjs/giggle/precache-manifest.7e9175df6f34245d04c0ec707cc0fa59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a0682196061ceff9335",
+    "url": "/portfolio/reactjs/giggle/static/js/runtime~main.3a068219.js"
+  },
+  {
+    "revision": "36b5e1ad326105d13782",
+    "url": "/portfolio/reactjs/giggle/static/js/main.36b5e1ad.chunk.js"
+  },
+  {
+    "revision": "8562945b9b82aeffe44a",
+    "url": "/portfolio/reactjs/giggle/static/js/1.8562945b.chunk.js"
+  },
+  {
+    "revision": "36b5e1ad326105d13782",
+    "url": "/portfolio/reactjs/giggle/static/css/main.7a3f7b0c.chunk.css"
+  },
+  {
+    "revision": "c5b2e26b45c7de1def88e149b353950c",
+    "url": "/portfolio/reactjs/giggle/index.html"
+  }
+];
