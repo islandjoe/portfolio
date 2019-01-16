9fc9be65748b5c422e5d28e188bb58873eb2c64b
Arthur Kho <islandjoe@gmail.com>
2019-01-16 14:00:51 +0200

    Giggle SPA

---
+++ reactjs/giggle/precache-manifest.ac6ba17ecf27f6f4fa26ecaa88525bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5404a2b44d91baff4331",
+    "url": "/static/js/main.5404a2b4.chunk.js"
+  },
+  {
+    "revision": "8562945b9b82aeffe44a",
+    "url": "/static/js/1.8562945b.chunk.js"
+  },
+  {
+    "revision": "5404a2b44d91baff4331",
+    "url": "/static/css/main.7a3f7b0c.chunk.css"
+  },
+  {
+    "revision": "c5c3914280c419ea7cd15e3cc88dde65",
+    "url": "/index.html"
+  }
+];
