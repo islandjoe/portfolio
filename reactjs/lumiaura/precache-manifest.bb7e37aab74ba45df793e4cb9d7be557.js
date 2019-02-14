be82b2cf552c02423e6545e1d8bb2fe8b92c831e
Arthur Kho <islandjoe@gmail.com>
2019-02-14 21:10:10 +0200

    Where the Snowplows At?

---
+++ reactjs/lumiaura/precache-manifest.bb7e37aab74ba45df793e4cb9d7be557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d683fed89140db43cbc",
+    "url": "/portfolio/reactjs/lumiaura/static/css/main.78f11850.chunk.css"
+  },
+  {
+    "revision": "8d683fed89140db43cbc",
+    "url": "/portfolio/reactjs/lumiaura/static/js/main.8d683fed.chunk.js"
+  },
+  {
+    "revision": "d51f18fd5d54001bd925",
+    "url": "/portfolio/reactjs/lumiaura/static/css/1.f0f90790.chunk.css"
+  },
+  {
+    "revision": "d51f18fd5d54001bd925",
+    "url": "/portfolio/reactjs/lumiaura/static/js/1.d51f18fd.chunk.js"
+  },
+  {
+    "revision": "4d4cb9382e0fb6ae73f5",
+    "url": "/portfolio/reactjs/lumiaura/static/js/runtime~main.4d4cb938.js"
+  },
+  {
+    "revision": "e86d585ea3e82d3471c44b5cf2ae275e",
+    "url": "/portfolio/reactjs/lumiaura/index.html"
+  }
+];
